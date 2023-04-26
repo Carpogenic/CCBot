f0ea5448d988db3059f97d38a30b073ffb2f8c05
Carpogenic <carpogenic@gmail.com>
2023-04-26 03:12:12 +0200

    Update events.py

--- attempt at organization/events.py
+++ attempt at organization/events.py
@@ -11,14 +11,17 @@ async def reaction_add_handler(bot, reaction, user, scores):
     if message.author == bot.user:
         # The bot reacted to its own message
         return
+    
     good_emoji = bot.get_emoji(1083983540124925952) # emoji ID
-    if reaction.emoji == good_emoji:
+    good_emoji2 = bot.get_emoji(1100587677193080882) # jank
+    if reaction.emoji in (good_emoji, good_emoji2):
         # Reacted with good emoji
         await message.add_reaction('👍')
         await update_scores(scores, message.author, user, 1)
 
     bad_emoji = bot.get_emoji(1083983552540053596)
-    if reaction.emoji == bad_emoji:
+    bad_emoji2 = bot.get_emoji(1100587719501029408)
+    if reaction.emoji in (bad_emoji, bad_emoji2):
         # Reacted with bad emoji
         await message.add_reaction('👎')
         await update_scores(scores, message.author, user, -1)
