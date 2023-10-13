91ee098a51dd97c8f5971882d6552ca83dbc35bd
Carpogenic <carpogenic@gmail.com>
2023-10-13 18:23:28 +0200

    fix bug causing songs not to queue properly

--- attempt at organization/bot_test.py
+++ attempt at organization/bot_test.py
@@ -9,6 +9,10 @@ import utils
 import events
 from commands.score import score_handler
 import typing
+import yt_dlp as yt_dlp
+from functools import partial
+import time
+
 
 intents = discord.Intents.default()
 intents.reactions = True
@@ -41,6 +45,153 @@ async def on_reaction_add(reaction, user):
 @bot.command()
 async def score(ctx, user: typing.Optional[discord.User] = None):
     await score_handler(ctx, bot, scores, user)
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+class Queue:
+    def __init__(self):
+        self._queue = []
+
+    def add(self, item):
+        self._queue.append(item)
+
+    def get_next(self):
+        return self._queue.pop(0) if self._queue else None
+
+    def is_empty(self):
+        return not bool(self._queue)
+
+
+song_queue = Queue()
+
+async def fetch_and_play(ctx, url, offset=0):
+    global start_time
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['url']
+
+            start_time = time.time()
+            print("play triggered")
+            ctx.voice_client.play(
+                discord.FFmpegPCMAudio(
+                    executable="ffmpeg",
+                    source=url2,
+                    before_options=f'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -ss {offset}'
+                ),
+                after=lambda e: bot.loop.create_task(after_play(e, ctx))
+            )
+            last_played["url"] = url
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
+async def play_next_song(ctx):
+    print("play_next_song triggered")
+    next_url = song_queue.get_next()
+    if next_url:
+        print(next_url)
+        await fetch_and_play(ctx, next_url)
+
+async def after_play(error, ctx):
+    print("after_play triggered")
+    if error:
+        print(f'Play error: {error}')
+    await play_next_song(ctx)
+
+last_played = {"url": None, "offset": 0}
+start_time = None
+accumulated_time = 0
+
+
+@bot.command()
+async def play(ctx, url=None):
+    global last_played, start_time, accumulated_time
+
+    if not url:
+        if last_played and last_played['url'] and not ctx.voice_client.is_playing():
+            url = last_played['url']
+            offset = last_played['offset'] + accumulated_time
+        else:
+            await ctx.send("No URL found to resume or queue, please provide a link.")
+            return
+    else:
+        # New song is being provided
+        offset = 0
+        accumulated_time = 0
+
+    # Check if the bot is either currently playing music or if there are songs queued up.
+    if (ctx.voice_client and (ctx.voice_client.is_playing() or not song_queue.is_empty()) and url):
+        song_queue.add(url)
+        await ctx.send(f"Song queued. Position: {len(song_queue._queue)}")
+        print(song_queue._queue)
+        return
+
+    
+    await fetch_and_play(ctx, url, offset)
+
+    
+
+
+@bot.command()
+async def stop(ctx):
+    global start_time, accumulated_time, last_played
+
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        # Calculate elapsed time from the last start_time and add to accumulated_time
+        elapsed_time = time.time() - start_time
+        accumulated_time += elapsed_time
+
+        # Update the offset with accumulated_time
+        last_played["offset"] += accumulated_time
+
+        # Reset accumulated_time for next play
+        accumulated_time = 0
+        
+        ctx.voice_client.stop()
+        await ctx.send("Stopped the audio playback.")
+    else:
+        await ctx.send("I'm not currently playing anything.")
+
+
+
+@bot.command()
+async def skip(ctx):
+    try:
+        ctx.voice_client.stop()
+        await play_next_song(ctx)
+        await ctx.send("Skipped to the next song.")
+    except Exception as e:
+        await ctx.send(f"Error in skip: {e}")
+
+help_text = """
+**Bot Commands:**
+`.join` - Makes the bot join the voice chat that you are in
+`.leave.` - Makes the bot disconnect from voice chat
+`.play <URL>` - Plays the song from the provided URL or resumes the last played song if no URL is given.
+`.stop` - Stops the currently playing song.
+`.skip` - Skips to the next song in the queue.
+"""
+
+@bot.command(name='halp')
+async def help_command(ctx):
+    await ctx.send(help_text)
+
+
 
 
 
